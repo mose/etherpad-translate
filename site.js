5a3cfd8f6b2afd5fdfb46305230d7293222cc75e
mose <mose@mose.com>
2015-08-06 19:00:41 -0400

    some styling

--- site.js
+++ site.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+  $.getScript("config.js.php");
   $(".nav li").click(function() {
     var pad = $(this).data('pad');
+    $(".original iframe").attr("src", "static_pad.php?pad="+pad);
+    $(".translated iframe").attr("src", epurl+"/p/"+pad.replace('en_', 'tw_')+"?showLineNumbers=false&showControls=false&useMonospaceFont=true&noColors=true&showChat=false");
     $(".nav li").removeClass('focus');
     $(this).addClass('focus');
   });
